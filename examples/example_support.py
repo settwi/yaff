f3e7d5ec3de2d30c1b6f84970fd0a57b37c55f0c
William Setterberg <sette095@umn.edu>
2025-05-09 13:51:41 -0500

    Fermi/GBM spectroscopy (#2)
    * Fermi fit WIP
    
    * rephrase comment
    
    * unit fix in common models
    
    * examples: use common_models.py

--- examples/example_support.py
+++ examples/example_support.py
@@ -3,27 +3,32 @@ from sunkit_spex.extern import rhessi
 import numpy as np
 from yaff import fitting
 
-def adapt_rhessi_data(rl: rhessi.RhessiLoader) -> fitting.DataPacket:
-    '''Convert the sunkit-spex format into a fitting.DataPacket'''
-    # One-D arrays for energy bins
-    count_ebins = np.unique(rl['count_channel_bins'].flatten()) << u.keV
-    photon_ebins = np.unique(rl['photon_channel_bins'].flatten()) << u.keV
 
-    cts = rl._loaded_spec_data['counts'] << u.ct # (rl._spectrum['counts'] / rl._spectrum['livetime'])
-    err = rl._loaded_spec_data['count_error'] << u.ct # (rl._spectrum['counts_err'] / rl._spectrum['livetime'])
+def adapt_rhessi_data(rl: rhessi.RhessiLoader) -> fitting.DataPacket:
+    """Convert the sunkit-spex format into a fitting.DataPacket"""
+    # One-D arrays for energy bins
+    count_ebins = np.unique(rl["count_channel_bins"].flatten()) << u.keV
+    photon_ebins = np.unique(rl["photon_channel_bins"].flatten()) << u.keV
+
+    cts = (
+        rl._loaded_spec_data["counts"] << u.ct
+    )  # (rl._spectrum['counts'] / rl._spectrum['livetime'])
+    err = (
+        rl._loaded_spec_data["count_error"] << u.ct
+    )  # (rl._spectrum['counts_err'] / rl._spectrum['livetime'])
 
     bkg_cts = (
-        rl['extras']['background_rate'] *
-        rl['count_channel_binning'] *
-        rl['effective_exposure']
+        rl["extras"]["background_rate"]
+        * rl["count_channel_binning"]
+        * rl["effective_exposure"]
     )
     bkg_err = (
-        rl['extras']['background_rate_error'] *
-        rl['count_channel_binning'] *
-        rl['effective_exposure']
+        rl["extras"]["background_rate_error"]
+        * rl["count_channel_binning"]
+        * rl["effective_exposure"]
     )
 
-    effective_exposure = rl._loaded_spec_data['effective_exposure'] << u.s
+    effective_exposure = rl._loaded_spec_data["effective_exposure"] << u.s
     return fitting.DataPacket(
         counts=cts << u.ct,
         counts_error=err << u.ct,
@@ -32,55 +37,5 @@ def adapt_rhessi_data(rl: rhessi.RhessiLoader) -> fitting.DataPacket:
         effective_exposure=effective_exposure << u.s,
         count_energy_edges=count_ebins << u.keV,
         photon_energy_edges=photon_ebins << u.keV,
-        response_matrix=rl['srm'].T << (u.cm**2 * u.ct / u.ph )
+        response_matrix=rl["srm"].T << (u.cm**2 * u.ct / u.ph),
     )
-
-
-def thermal_and_thick(arg_dict: dict[str, object]):
-    # Imports need to be inside the model
-    # function for pickling/multiprocessing
-    from sunkit_spex.legacy import thermal
-    from sunkit_spex.legacy import emission
-
-    # The dict type annotation in the function
-    # declaration is ambiguous; so, annotate the variables here
-    ph_edges: np.ndarray = arg_dict['photon_energy_edges']
-    params: dict[str, fitting.Parameter] = arg_dict['parameters']
-
-    # Hack the thermal emission to work with
-    # the RHESSI energy range (...)
-    lowe, highe = thermal.CONTINUUM_GRID['energy range keV']
-    ph_mids = ph_edges[:-1] + np.diff(ph_edges)/2
-    kept = (ph_edges >= lowe) & (ph_edges <= highe)
-    kept_mids = (ph_mids >= lowe) & (ph_mids <= highe)
-
-    thermal_portion = thermal.thermal_emission(
-        energy_edges=ph_edges[kept] << u.keV,
-        temperature=params['temperature'].as_quantity(),
-        emission_measure=params['emission_measure'].as_quantity()
-    ).to_value(u.ph / u.s / u.keV / u.cm**2)
-
-    # Zero-out the data which falls outside the
-    # normal thermal energy grid
-    thermal_fixed = np.zeros(kept.size - 1)
-    thermal_fixed[kept_mids] = thermal_portion
-    thermal_portion = thermal_fixed
-
-    # We have to evaluate the thick target model at energy midpoints,
-    # so compute them here
-    ph_mids = ph_edges[:-1] + np.diff(ph_edges)/2
-
-    # Single power law; very high energy break
-    break_energy = 100 * ph_mids[-1]
-    nonthermal_portion = emission.bremsstrahlung_thick_target(
-        photon_energies=ph_mids,
-        p=params['spectral_index'].value,
-        q=0,
-        eelow=params['cutoff_energy'].value,
-        eebrk=break_energy,
-        eehigh=break_energy,
-    )
-
-    # For this model, assume earth observer distance
-    scaled_flux = params['electron_flux'].as_quantity().to_value(1e35 * u.electron / u.s)
-    return thermal_portion + (1e35 * scaled_flux * nonthermal_portion)
---
+++ yaff/common_models.py
@@ -0,0 +1,89 @@
+"""Common models used when fitting X-ray data"""
+from typing import TypeAlias
+import astropy.units as u
+from yaff import fitting
+import numpy as np
+
+# The type annotation for what's passed to different functions
+ArgsT: TypeAlias = dict[str, np.ndarray | dict[str, fitting.Parameter]]
+
+
+def thermal(args: dict[str, ArgsT]):
+    '''Thermal bremsstrahlung emission, assuming coronal densities.'''
+    from sunkit_spex.legacy import thermal
+
+    params: dict[str, fitting.Parameter] = args['parameters']
+    temperature = params['temperature']
+    emission_measure = params['emission_measure']
+
+    ph_edges: np.ndarray = args['photon_energy_edges']
+    
+    # Zero out anything outside the range of the current grid
+    lowe, highe = thermal.CONTINUUM_GRID['energy range keV']
+    ph_mids = ph_edges[:-1] + np.diff(ph_edges)/2
+    kept = (ph_edges >= lowe) & (ph_edges <= highe)
+    kept_mids = (ph_mids >= lowe) & (ph_mids <= highe)
+
+    thermal_portion = thermal.thermal_emission(
+        energy_edges=ph_edges[kept] << u.keV,
+        temperature=temperature.as_quantity(),
+        emission_measure=emission_measure.as_quantity()
+    ).to_value(u.ph / u.s / u.keV / u.cm**2)
+
+    # Perform the zeroing-out
+    thermal_fixed = np.zeros(kept.size - 1)
+    thermal_fixed[kept_mids] = thermal_portion
+    thermal_portion = thermal_fixed
+    return thermal_portion
+
+
+def thick_target(args: dict[str, ArgsT]):
+    '''A single power law thick target emission function.'''
+    from sunkit_spex.legacy import emission
+
+    ph_edges: np.ndarray = args['photon_energy_edges']
+    # We have to evaluate the thick target model at energy midpoints,
+    # so compute them here
+    ph_mids = ph_edges[:-1] + np.diff(ph_edges)/2
+
+    params: dict[str, fitting.Parameter] = args['parameters']
+    spectral_index = params['spectral_index']
+    cutoff_energy = params['cutoff_energy']
+
+    # Single power law; very high energy break
+    break_energy = 100 * ph_mids[-1]
+    nonthermal_portion = emission.bremsstrahlung_thick_target(
+        photon_energies=ph_mids,
+        p=spectral_index.value,
+        q=0,
+        eelow=cutoff_energy.value,
+        eebrk=break_energy,
+        eehigh=break_energy,
+    )
+
+    # For this model, assume earth observer distance
+    electron_flux = params['electron_flux']
+    scaled_flux = electron_flux.as_quantity().to_value(u.electron / u.s)
+    return scaled_flux * nonthermal_portion
+
+
+def broken_power_law(args: dict):
+    '''a power law in photon space.
+       param names for the dictionary:
+       - norm_energy (usually fixed)
+       - norm_flux
+       - break_energy
+       - lower_index (sometimes fixed)
+       - upper_index
+    '''
+    from sunkit_spex.legacy import photon_power_law as ppl
+    edges = args['photon_energy_edges'] << u.keV
+    params = args['parameters']
+    return ppl.compute_broken_power_law(
+        edges,
+        norm_energy=params['norm_energy'].as_quantity(),
+        norm_flux=params['norm_flux'].as_quantity(),
+        break_energy=params['break_energy'].as_quantity(),
+        lower_index=params['lower_index'].as_quantity(),
+        upper_index=params['upper_index'].as_quantity(),
+    ).to_value(u.ph / u.keV / u.cm**2 / u.s)
